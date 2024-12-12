85e192b64704d8ae0a41f908bd906da65da6388e
Sam Radford <smr1292@yahoo.com>
2024-12-12 11:52:43 -0500

    Adding base url to tsconfig and adding some routing to move content from app.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: "home"},
+    { path: 'home', 
+      children: [
+        { path: '', pathMatch: 'full', component: HomeComponent},
+      ]
+    },
+    { path: '**', redirectTo: "home"}
+
+];
